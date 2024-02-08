cfb7909f77444d9d25ec2e3a6c3618eaa4b2f61e
Onyekwere Michael <onyekweremichael55@gmail.com>
2024-02-08 03:04:59 -0800

    open

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,22 @@ import { useState } from 'react'
 import systemicon from "./assets/systemicon.png"
 import system from "./assets/system.jpg"
 import { FaCartPlus, FaMailBulk, FaTicketAlt, FaFacebook, FaTwitter, FaInstagram} from "react-icons/fa";
-
-
+import { movies } from "../src/mapped";
+import { motion} from "framer-motion"
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
 function App() {
+  const conitaner = {
+    hidden: {
+      y: 800, opacity: 0
+    },
+    visible:{
+      y: 0, opacity: 1,
+      transition:{
+        duration: 1.6, ease: "easeInOut",
+      }
+    }
+  }
   return (
       <header>
         <section className='overall_header'>
@@ -28,7 +41,7 @@ function App() {
           technical skills, creativity, and a commitment to continuous learning.
           Here are ten tips to help you thrive in this dynamic field:
         </p>
-        <button class="tricks">Tips & Tricks</button>
+        <button class="tips">Tips & Tricks</button>
       </div>
         <img src={system} alt="" class="dev"/>
     </section>
@@ -36,12 +49,105 @@ function App() {
 
           <main>
             <section className='overall_main'>
-                <FaFacebook className="facebook"/>
-                 <p>Lorem ipsum dolor, sit amet<br/> consectetur adipisicing elit.<br/> Temporibus tempore, quo faci</p>
-                 <FaTwitter className="twitter"/>
-                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus aperiam molestias quae harum provident perspiciatis amet nihil facere veritatis corrupti ab, ad atque. Consequuntur eaque sit nobis perspiciatis sed nihil!</p>
+              <img src={system} alt="" className='dev'/>
+                 <div>
+                   <h2>Tips to become a Dev</h2>
+                   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga quae quidem in provident, adipisci minima. Dolores aperiam minima explicabo impedit reprehenderit, ratione facilis amet at obcaecati doloribus, odio alias? Porro?</p>
+                   <button>Tips & Tricks</button>
+                   </div>
+                 </section>
+
+                 <section>
+                      <p>Sevices</p>
+                 </section>
+
+                 <section>
+                 <Swiper
+        className="mySwiper"
+        spaceBetween={1}
+        slidesPerView={1}
+        breakpoints={{
+          540: {
+            slidesPerView: 1,
+          },
+          600: {
+            slidesPerView: 3,
+          },
+          1000: {
+            slidesPerView: 4,
+          },
+        }}
+      >
+        {movies.map((item, ivx) => (
+          <div key={ivx}>
+            <SwiperSlide>
+              <div className="swipers">
+                <img
+                  src={item.img}
+                  alt=""
+                  className="services_img"
+                />
+                <h1>{item.name}</h1>
+                <p>
+                  {item.text}
+                </p>
+                <button>{item.button}</button>
+              </div>
+            </SwiperSlide>
+          </div>
+        ))}
+      </Swiper>
                  </section>
           </main>
+
+          {/* footer stying begins */}
+
+          <motion.footer
+            variants={conitaner}
+            initial= "hidden"
+            whileInView= "visible"
+            transition={{duration: 0.6, delay: 0.23, ease: "easeInOut"}}
+            viewport={{
+              once: true
+            }}
+    >
+      <section class="white">
+        <h1 class="veta mar">MetaV</h1>
+        <p>
+          Adipiscing at in tellus integer. Pellentesque massa<br />
+          placerat duis ultricies lacus. Nisi porta lorem mollis<br />
+          aliquam ut porttitor
+        </p>
+      </section>
+
+      <section class="white">
+        <p>About Us</p>
+        <p>About MetaV</p>
+        <p>Contact Us</p>
+        <p>FAQS</p>
+        <p>privacy Policies</p>
+        <p>Terms of policies</p>
+      </section>
+
+      <section class="white">
+        <p>Services</p>
+        <p>NFT marketplace</p>
+        <p>Virtual Concert</p>
+        <p>Gaming</p>
+        <p>Artwork</p>
+        <p>Online Services</p>
+      </section>
+
+      <section class="white">
+        <p>Contact</p>
+        <span><img src="assesets/LinkedIn.svg" alt="" class="imj" /></span>
+        <span><img src="assesets/Twitter Squared.svg" alt="" /></span>
+        <span><img src="assesets/Instagram.svg" alt="" /></span>
+        <span><img src="assesets/Facebook.svg" alt="" /></span>
+        <p>2022 © MetaV.com All right reserved.</p>
+      </section>
+    </motion.footer>
+
   </header>
   )
 }
---
+++ src/mapped.jsx
@@ -0,0 +1,41 @@
+import system from "./assets/system.jpg"
+
+export const movies = [
+        {
+            img: system,
+            name: "Audit",
+          text: " We want you to enjoy your travels.This is why we don't change any fees on purchase while you'reabroad. We'even show you...",
+            button: "Experienced Team",
+        },
+        {
+            img: system,
+            name: "Teamwork",
+          text: " We want you to enjoy your travels.This is why we don't change any fees on purchase while you'reabroad. We'even show you...",
+            button: "Experienced Team",
+        },
+        {
+            img: system,
+            name: "Create",
+          text: " We want you to enjoy your travels.This is why we don't change any fees on purchase while you'reabroad. We'even show you...",
+            button: "Experienced Team",
+        },
+        {
+            img: system,
+            name: "Audit",
+          text: " We want you to enjoy your travels.This is why we don't change any fees on purchase while you'reabroad. We'even show you...",
+            button: "Experienced Team",
+        },
+        {
+            img: system,
+          name: "Audit",
+          text: " We want you to enjoy your travels.This is why we don't change any fees on purchase while you'reabroad. We'even show you...",
+          button: "Experienced Team",
+        },
+        {
+            img: system,
+            name: "Audit",
+          text: " We want you to enjoy your travels.This is why we don't change any fees on purchase while you'reabroad. We'even show you...",
+            button: "Experienced Team",
+        },
+       
+    ]
